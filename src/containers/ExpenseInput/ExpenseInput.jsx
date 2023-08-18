cbf7021423f3af8ca2475628e0247a34e543d329
Monica Calderaro <mcalderarorey@gmail.com>
2023-08-18 12:14:22 +0200

    layout and creating a store

--- src/containers/ExpenseInput/ExpenseInput.jsx
+++ src/containers/ExpenseInput/ExpenseInput.jsx
@@ -1,8 +1,20 @@
+import { addExpenseAction } from "store/expense/expense-slice";
 import s from "./style.module.css";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export function ExpenseInput(props) {
+  const [expenseName, setExpenseName] = useState();
+  const [price, setPrice] = useState();
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addExpenseAction({ name: expenseName, price: price }));
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
           <input
@@ -10,6 +22,7 @@ export function ExpenseInput(props) {
             className="form-control"
             placeholder='Ex : "Apple"'
             name="name"
+            onChange={(e) => setExpenseName(e.target.value)}
           />
         </div>
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
@@ -19,6 +32,7 @@ export function ExpenseInput(props) {
             className="form-control"
             placeholder="Ex: 3.99"
             name="price"
+            onChange={(e) => setPrice(e.target.value)}
           />
         </div>
 
